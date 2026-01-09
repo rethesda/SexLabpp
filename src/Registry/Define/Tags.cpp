89d1938c1633c520b2753d5afbafd96c2b198bfd
UaineMazoku <107775709+UaineMazoku@users.noreply.github.com>
2026-01-08 21:52:39 -0500

    fix: tag matching regression when excluding base tags

--- src/Registry/Define/Tags.cpp
+++ src/Registry/Define/Tags.cpp
@@ -164,10 +164,10 @@ namespace Registry
 						 std::ranges::all_of(a_tag._extratags, [&](const auto& tag) { return HasTag(tag); }) &&
 						 std::ranges::all_of(a_tag._annotations, [&](const auto& tag) { return HasTag(tag); });
 		} else {
-			if (_basetags.any(a_tag._basetags.get())) return true;
-			if (a_tag._extratags.empty() && a_tag._annotations.empty()) return true;
-			return std::ranges::any_of(a_tag._extratags, [&](const auto& tag) { return HasTag(tag); }) ||
-						 std::ranges::any_of(a_tag._annotations, [&](const auto& tag) { return HasTag(tag); });
+			return a_tag.IsEmpty() ||
+				   _basetags.any(a_tag._basetags.get()) ||
+				   std::ranges::any_of(a_tag._extratags, [&](const auto& tag) { return HasTag(tag); }) ||
+				   std::ranges::any_of(a_tag._annotations, [&](const auto& tag) { return HasTag(tag); });
 		}
 	}
 
